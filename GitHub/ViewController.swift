aed0a6da914db252a9a6d95cf5a1fbf4223eab9a
TaniguchiSota <taniguchi1412@gmail.com>
2020-12-13 08:45:47 +0900

    Initial Commit

---
+++ GitHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHub
+//
+//  Created by 谷口　颯汰 on 2020/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
